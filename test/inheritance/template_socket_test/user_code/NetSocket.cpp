a2ea73ce0b640bf0a896a87307ec9f75afe9a706
DIvanchykhin <di@o-log-n.com>
2018-08-22 13:39:23 +0300

    code unification for inheritance and template-based tests

--- test/inheritance/template_socket_test/user_code/NetSocket.cpp
+++ test/inheritance/template_socket_test/user_code/NetSocket.cpp
@@ -4,29 +4,28 @@
 #include "../../../../src/infrastructure.h"
 #include "NetSocket.h"
 
-#ifdef USING_L_SOCKETS
-static NodeRegistrator<MySampleLambdaOneNode> noname( "MySampleLambdaOneNode" );
-#endif // USING_L_SOCKETS
-#ifdef USING_O_SOCKETS
+#if (defined USING_L_SOCKETS) || (defined USING_O_SOCKETS)
+//static NodeRegistrator<MySampleLambdaOneNode> noname( "MySampleLambdaOneNode" );
 static NodeRegistrator<MySampleInheritanceOneNode> noname( "MySampleInheritanceOneNode" );
-#endif // USING_O_SOCKETS
+#endif // USING_L_SOCKETS || USING_O_SOCKETS
 
 
 #ifdef USING_T_SOCKETS
+
 static thread_local Infrastructure<MySampleTNode::EmitterType> infra;
 
 size_t connectToInfra(net::SocketTBase* t, int typeId, const char* ip, uint16_t port)
 {
 	return infra.getNetSocket().appConnect(t, typeId, ip, port);
 }
-#endif // USING_T_SOCKETS
 
 void runLoop()
 {
 	MySampleTNode node;
 	node.main();
-	void* nodePtr = nullptr;
 	runInfraLoop2( infra );
 }
 
 NetSocketManagerBase& getNetSocket() { return infra.getNetSocket(); }
+
+#endif // USING_T_SOCKETS
--- tests/experimental/lambda_test_one/user_code/NetSocket.cpp
+++ tests/experimental/lambda_test_one/user_code/NetSocket.cpp
@@ -1,9 +1,31 @@
 // NetSocket.cpp : sample of user-defined code
 
 
+#include "../../../../src/infrastructure.h"
 #include "NetSocket.h"
 
+#if (defined USING_L_SOCKETS) || (defined USING_O_SOCKETS)
 //static NodeRegistrator<MySampleLambdaOneNode> noname( "MySampleLambdaOneNode" );
-#ifdef USING_O_SOCKETS
 static NodeRegistrator<MySampleInheritanceOneNode> noname( "MySampleInheritanceOneNode" );
-#endif // USING_O_SOCKETS
+#endif // USING_L_SOCKETS || USING_O_SOCKETS
+
+
+#ifdef USING_T_SOCKETS
+
+static thread_local Infrastructure<MySampleTNode::EmitterType> infra;
+
+size_t connectToInfra(net::SocketTBase* t, int typeId, const char* ip, uint16_t port)
+{
+	return infra.getNetSocket().appConnect(t, typeId, ip, port);
+}
+
+void runLoop()
+{
+	MySampleTNode node;
+	node.main();
+	runInfraLoop2( infra );
+}
+
+NetSocketManagerBase& getNetSocket() { return infra.getNetSocket(); }
+
+#endif // USING_T_SOCKETS
