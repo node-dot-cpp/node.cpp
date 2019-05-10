30a72481ffa0590840a22f69a9498a386a447650
DIvanchykhin <di@o-log-n.com>
2019-05-10 15:38:28 +0300

    minimalistic implementation of o_awaitable_client

--- test/samples/o_awaitable_client/user_code/NetSocket.h
+++ test/samples/o_awaitable_client/user_code/NetSocket.h
@@ -33,7 +33,6 @@ public:
 		nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "MySampleLambdaOneNode::main()" );
 
 		*( clientSock.getExtra() ) = 17;
-		//clientSock.connect(2000, "127.0.0.1");
 		co_await clientSock.a_connect(2000, "127.0.0.1");
 		buf.writeInt8( 2, 0 );
 		buf.writeInt8( 1, 1 );
@@ -42,52 +41,6 @@ public:
 		co_await doWhateverWithIncomingData();
 		co_return;
 	}
-	
-	void onWhateverConnect(nodecpp::safememory::soft_ptr<nodecpp::net::SocketOUserBase<MySampleTNode,SocketIdType>> socket) 
-	{
-		buf.writeInt8( 2, 0 );
-		buf.writeInt8( 1, 1 );
-		socket->write(buf);
-	}
-
-	void onWhateverData(nodecpp::safememory::soft_ptr<nodecpp::net::SocketOUserBase<MySampleTNode,SocketIdType>> socket, nodecpp::Buffer& buffer)
-	{
-		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, socket );
-		++recvReplies;
-		if ( ( recvReplies & 0xFFFF ) == 0 )
-//			nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "[{}] MySampleTNode::onData(), size = {}", recvReplies, buffer.size() );
-			nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "[{}] MySampleTNode::onWhateverData(), extra = {}, size = {}", recvReplies, *(socket->getExtra()), buffer.size() );
-		recvSize += buffer.size();
-		buf.writeInt8( 2, 0 );
-		buf.writeInt8( (uint8_t)recvReplies | 1, 1 );
-		socket->write(buf);
-	}
-
-	/*awaitable<void> incomingDataLoop(nodecpp::safememory::soft_ptr<nodecpp::net::SocketOUserBase<MySampleTNode,SocketIdType>> socket)
-	{
-		for (;;)
-		{
-			nodecpp::Buffer r_buff(0x100);
-			try
-			{
-				co_await socket->a_read(r_buff);
-			}
-			catch (...)
-			{
-				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::error>("Reading data failed (extra = {}). Exiting...", *(socket->getExtra()));
-				break;
-			}
-			NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, socket );
-			++recvReplies;
-			if ( ( recvReplies & 0xFFF ) == 0 )
-				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "[{}] MySampleTNode::onWhateverData(), size = {}, total received size = {}", recvReplies, r_buff.size(), recvSize );
-			recvSize += r_buff.size();
-			buf.writeInt8( 2, 0 );
-			buf.writeInt8( (uint8_t)recvReplies | 1, 1 );
-			socket->write(buf);
-		}
-		co_return;
-	}*/
 
 	awaitable<void> doWhateverWithIncomingData()
 	{
@@ -103,7 +56,6 @@ public:
 				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::error>("Reading data failed (extra = {}). Exiting...", *(clientSock.getExtra()));
 				break;
 			}
-//			size_t read_ret = co_await clientSock.read();
 			++recvReplies;
 			if ( ( recvReplies & 0xFFF ) == 0 )
 				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "[{}] MySampleTNode::onWhateverData(), size = {}, total received size = {}", recvReplies, r_buff.size(), recvSize );
@@ -124,11 +76,7 @@ public:
 		co_return;
 	}
 
-	using ClientSockType = nodecpp::net::SocketN<MySampleTNode,SocketIdType,
-		nodecpp::net::OnConnect<&MySampleTNode::onWhateverConnect>/*,
-		nodecpp::net::OnData<&MySampleTNode::onWhateverData>,
-		nodecpp::net::OnDataAwaitable<&MySampleTNode::incomingDataLoop>*/
-	>;
+	using ClientSockType = nodecpp::net::SocketN<MySampleTNode,SocketIdType>;
 	ClientSockType clientSock;
 
 	using EmitterType = nodecpp::net::SocketTEmitter<net::SocketO, net::Socket>;
