dcea5a1bd1c07f1da759f3e8a70dce369372b535
dmytro.ivanchykhin <di@o-log-n.com>
2019-07-10 20:34:18 +0300

    removing outdated test apps

--- test/samples/o_awaitable_client/user_code/NetSocket.cpp
+++
@@ -1,7 +0,0 @@
-// NetSocket.cpp : sample of user-defined code
-
-
-#include "../../../../src/infrastructure.h"
-#include "NetSocket.h"
-
-static NodeRegistrator<Runnable<MySampleTNode>> noname( "MySampleTemplateNode" );
