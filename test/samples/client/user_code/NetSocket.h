44ff75f60e474bd2fb2e780c4cbf941f172294f9
dmytro.ivanchykhin <di@o-log-n.com>
2019-07-12 18:24:57 +0300

    yet sample code for client

--- test/samples/client/user_code/NetSocket.h
+++ test/samples/client/user_code/NetSocket.h
@@ -12,8 +12,9 @@ using namespace nodecpp;
 using namespace fmt;
 
 #ifndef NODECPP_NO_COROUTINES
-#define IMPL_VERSION 2 // main() is a single coro
-//#define IMPL_VERSION 3 // onConnect is a coro
+//#define IMPL_VERSION 2 // main() is a single coro
+#define IMPL_VERSION 21 // main() is a single coro with non-default socket class
+//#define IMPL_VERSION 3 // onConnect is a coro (onConnect is added via addHandler<...>(...))
 //#define IMPL_VERSION 4 // registering handlers (per class)
 //#define IMPL_VERSION 5 // registering handlers (per class, template-based)
 //#define IMPL_VERSION 6 // registering handlers (per class, template-based) with no explicit awaitable staff
@@ -100,6 +101,88 @@ public:
 			catch (...)
 			{
 				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::error>("Writing data failed). Exiting...");
+				break;
+			}
+			// TODO: address failure
+		}
+		CO_RETURN;
+	}
+
+	using EmitterType = nodecpp::net::SocketTEmitter<>;
+
+#elif IMPL_VERSION == 21
+
+	class MySocketOne : public nodecpp::net::SocketBase
+	{
+		int extraData;
+
+	public:
+		MySocketOne() {}
+		virtual ~MySocketOne() {}
+
+		int* getExtra() { return &extraData; }
+	};
+
+	virtual nodecpp::handler_ret_type main()
+	{
+		nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "MySampleLambdaOneNode::main()" );
+
+		clientSock = nodecpp::net::createSocket<ClientSockType>();
+		*( clientSock->getExtra() ) = 17;
+
+		try
+		{
+			co_await clientSock->a_connect(2000, "127.0.0.1");
+			buf.writeInt8( 2, 0 );
+			buf.writeInt8( 1, 1 );
+			co_await clientSock->a_write(buf);
+			// TODO: address failure
+			co_await doWhateverWithIncomingData(clientSock);
+		}
+		catch (...)
+		{
+			nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::error>("processing on socket with extra = {} failed. Exiting...", *(clientSock->getExtra()));
+		}
+		CO_RETURN;
+	}
+
+	using ClientSockType = MySocketOne;
+
+	awaitable<void> doWhateverWithIncomingData(nodecpp::safememory::soft_ptr<ClientSockType> socket)
+	{
+		for (;;)
+		{
+			nodecpp::Buffer r_buff(0x200);
+			try
+			{
+				co_await socket->a_read(r_buff);
+			}
+			catch (...)
+			{
+				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::error>("Reading data failed (extra = {}). Exiting...", *(socket->getExtra()));
+				break;
+			}
+			++recvReplies;
+#ifdef AUTOMATED_TESTING_ONLY
+			if ( recvReplies > AUTOMATED_TESTING_CYCLE_COUNT )
+			{
+				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "About to exit successfully in automated testing" );
+				end();
+				unref();
+			}
+#endif
+			if ( ( recvReplies & 0xFFF ) == 0 )
+				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::info>( "[{}] MySampleTNode::onWhateverData(), size = {}, total received size = {}", recvReplies, r_buff.size(), recvSize );
+			recvSize += r_buff.size();
+			buf.writeInt8( 2, 0 );
+			buf.writeInt8( (uint8_t)recvReplies | 1, 1 );
+			try
+			{
+				co_await socket->a_write(buf);
+			}
+			catch (...)
+			{
+				nodecpp::log::log<nodecpp::module_id, nodecpp::log::LogLevel::error>("Writing data failed (extra = {}). Exiting...", *(socket->getExtra()));
 				break;
 			}
 			// TODO: address failure
