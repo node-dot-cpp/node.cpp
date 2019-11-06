2b2cd9c73f2e5858c62cfaeeb0ab080cf8a9555b
DIvanchykhin <di@o-log-n.com>
2019-11-06 16:04:52 +0200

    automated releasing of promise object (removing excessive log output)

--- include/nodecpp/awaitable.h
+++ include/nodecpp/awaitable.h
@@ -100,21 +100,16 @@ struct promise_type_struct_base {
 	~promise_type_struct_base() {}
 
     auto initial_suspend() {
-//            return std::experimental::suspend_always{};
         return std::experimental::suspend_never{};
     }
 	auto final_suspend() {
-//printf( "promise_type_struct_base::final_suspend(), this = 0x%zx [2], refCtr = %zd\n", (size_t)(this), refCtr );
-printf( "promise_type_struct_base::final_suspend(), this = 0x%zx [2]\n", (size_t)(this) );
 		if ( hr )
 		{
 			auto tmph = hr;
 			hr = nullptr;
 			tmph();
 		}
-//			return std::experimental::suspend_always{};
 		return std::experimental::suspend_never{};
-//		return std::experimental::suspend_if{refCtr != 0};
     }
 };
 
@@ -126,12 +121,12 @@ struct promise_type_struct : public promise_type_struct_base {
 
 	awaitable<T>* myRetObject = nullptr;
 
-	promise_type_struct() : promise_type_struct_base() {printf( "promise_type_struct(), this = 0x%zx [2]\n", (size_t)(this));}
+	promise_type_struct() : promise_type_struct_base() {}
 	promise_type_struct(const promise_type_struct &) = delete;
 	promise_type_struct &operator = (const promise_type_struct &) = delete;
 	promise_type_struct(promise_type_struct &&) = delete;
 	promise_type_struct &operator = (promise_type_struct &&) = delete;
-	~promise_type_struct()  {printf( "~promise_type_struct(), this = 0x%zx [2]\n", (size_t)(this));}
+	~promise_type_struct()  {}
 
     auto get_return_object();
     auto return_value(T v);
@@ -144,12 +139,12 @@ struct promise_type_struct<void> : public promise_type_struct_base {
 
 	awaitable<void>* myRetObject = nullptr;
 
-	promise_type_struct() : promise_type_struct_base() {printf( "promise_type_struct<void>(), this = 0x%zx\n", (size_t)(this));}
+	promise_type_struct() : promise_type_struct_base() {}
 	promise_type_struct(const promise_type_struct &) = delete;
 	promise_type_struct &operator = (const promise_type_struct &) = delete;
 	promise_type_struct(promise_type_struct &&) = delete;
 	promise_type_struct &operator = (promise_type_struct &&) = delete;
-	~promise_type_struct() {printf( "~promise_type_struct<void>(), this = 0x%zx\n", (size_t)(this));}
+	~promise_type_struct() {}
 
     auto get_return_object();
 	auto return_void(void);
@@ -159,14 +154,12 @@ struct promise_type_struct<void> : public promise_type_struct_base {
 
 inline
 void setNoException(std::experimental::coroutine_handle<> awaiting) {
-	printf( "setNoException(), awaiting = 0x%zx[3]\n", (size_t)(awaiting.address()));
 	auto& edata = std::experimental::coroutine_handle<nodecpp::promise_type_struct<void>>::from_address(awaiting.address()).promise().edata;
 	edata.is_exception = false;
 }
 
 inline
 void setException(std::experimental::coroutine_handle<> awaiting, std::exception e) {
-	printf( "setException(), awaiting = 0x%zx[3]\n", (size_t)(awaiting.address()));
 	auto& edata = std::experimental::coroutine_handle<nodecpp::promise_type_struct<void>>::from_address(awaiting.address()).promise().edata;
 	edata.is_exception = true;
 	edata.exception = e;
@@ -174,14 +167,12 @@ void setException(std::experimental::coroutine_handle<> awaiting, std::exception
 
 inline
 bool isException(std::experimental::coroutine_handle<> awaiting) {
-	printf( "isException(), awaiting = 0x%zx[3]\n", (size_t)(awaiting.address()));
 	auto& edata = std::experimental::coroutine_handle<nodecpp::promise_type_struct<void>>::from_address(awaiting.address()).promise().edata;
 	return edata.is_exception;
 }
 
 inline
 std::exception& getException(std::experimental::coroutine_handle<> awaiting) {
-	printf( "getException(), awaiting = 0x%zx[3]\n", (size_t)(awaiting.address()));
 	auto& edata = std::experimental::coroutine_handle<nodecpp::promise_type_struct<void>>::from_address(awaiting.address()).promise().edata;
 	return edata.exception;
 }
@@ -197,10 +188,9 @@ struct awaitable : public awaitable_base  {
 	bool coroDestroyed = false;
 	using value_type = T;
 
-	awaitable()  {printf( "awaitable(), this = 0x%zx\n", (size_t)(this));}
+	awaitable()  {}
 	awaitable(handle_type h) : coro(h) { 
 		coro.promise().myRetObject = this;
-		printf( "awaitable(), this = 0x%zx, coro = 0x%zx[2]\n", (size_t)(this), (size_t)(coro.address()) - 0x80 );
 	}
 
     awaitable(const awaitable &) = delete;
@@ -208,23 +198,16 @@ struct awaitable : public awaitable_base  {
 
 	awaitable(awaitable &&s) : coro(s.coro) {
 		s.coro = nullptr;
-		printf( "awaitable(), this = 0x%zx, coro = 0x%zx, from awaitable = 0x%zx[3]\n", (size_t)(this)), (size_t)(coro.address(), &s);
 	}
 	awaitable &operator = (awaitable &&s) {
 		coro = s.coro;
 		s.coro = nullptr;
-		printf( "awaitable(), this = 0x%zx, coro = 0x%zx, from awaitable = 0x%zx[3]\n", (size_t)(this)), (size_t)(coro.address(), &s);
 		return *this;
 	}   
 	
 	~awaitable() {
 		if ( !coroDestroyed )
-		{
-			printf( "    ~awaitable(): dereferencing coro 0x%zx\n", (size_t)(coro.address()) - 0x80 );
 			coro.promise().myRetObject = nullptr;
-		}
-		else
-			printf( "~awaitable(), this = 0x%zx\n", (size_t)(this) );
 	}
 
 	typename void_type_converter<T>::type& getValue() {
@@ -235,7 +218,6 @@ struct awaitable : public awaitable_base  {
 	}
 
 	typename void_type_converter<T>::type get() {
-		printf( "get(), this = 0x%zx\n", (size_t)(this));
 		if constexpr ( std::is_same<void, T>::value )
 			return placeholder_for_void_ret_type();
 		else
@@ -243,16 +225,13 @@ struct awaitable : public awaitable_base  {
 	}
 
 	bool await_ready() noexcept { 
-		printf( "await_ready(), this = 0x%zx\n", (size_t)(this));
 		return is_value;		
 	}
 	void await_suspend(std::experimental::coroutine_handle<> h_) noexcept {
-		printf( "await_suspend(), this = 0x%zx, coro? %s\n", (size_t)(this), coro == nullptr ? "NO" : "Yes" );
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, !coroDestroyed ); 
 		coro.promise().hr = h_;
 	}
 	typename void_type_converter<T>::type await_resume() { 
-		printf( "await_resume(), this = 0x%zx\n", (size_t)(this));
 		if ( e_pending != nullptr )
 		{
 			std::exception_ptr ex = e_pending;
@@ -264,20 +243,16 @@ struct awaitable : public awaitable_base  {
 		else
 			return this->getValue();
 	}
-
 };
 
 inline
 auto promise_type_struct<void>::get_return_object() {
-		printf( "promise_type_struct<void>::get_return_object(), this = 0x%zx\n", (size_t)(this));
 		auto h = handle_type::from_promise(*this);
 		return awaitable<void>{h};
 }
 
 template<class T>
 auto promise_type_struct<T>::get_return_object() {
-		printf( "promise_type_struct<T>::get_return_object(), this = 0x%zx\n", (size_t)(this));
-		printf( "await_resume(), this = 0x%zx\n", (size_t)(this));
 		auto h = handle_type::from_promise(*this);
 		return awaitable<T>{h};
 }
@@ -292,24 +267,21 @@ auto promise_type_struct<T>::return_value(T v) {
     return std::experimental::suspend_never{};
 }
 
+inline
+auto promise_type_struct<void>::return_void(void) {
+	if ( myRetObject != nullptr )
+		myRetObject->is_value = true;
+	return std::experimental::suspend_never{};
+}
+
 template<class T>
 void promise_type_struct<T>::unhandled_exception() {
-printf( "promise_type_struct::unhandled_exception(), this = 0x%zx [2]\n", (size_t)(this));
 	if ( myRetObject != nullptr )
 		myRetObject->e_pending = std::current_exception();
 }
 
 inline
-auto promise_type_struct<void>::return_void(void) {
-printf( "promise_type_struct::return_void(), this = 0x%zx\n", (size_t)(this));
-if ( myRetObject != nullptr )
-	myRetObject->is_value = true;
-    return std::experimental::suspend_never{};
-}
-
-inline
 void promise_type_struct<void>::unhandled_exception() {
-printf( "promise_type_struct<void>::unhandled_exception(), this = 0x%zx [2]\n", (size_t)(this));
 	if ( myRetObject != nullptr )
 		myRetObject->e_pending = std::current_exception();
 }
