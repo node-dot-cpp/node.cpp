c6465faf428b2188d4a2e318561319f78a152579
dmytro.ivanchykhin <di@o-log-n.com>
2020-01-21 17:07:40 +0200

    cleaning up samples

--- samples/http_server_coro/user_code/NetSocket.cpp
+++
@@ -1,7 +0,0 @@
-// NetSocket.cpp : sample of user-defined code
-
-
-#include <infrastructure.h>
-#include "NetSocket.h"
-
-static NodeRegistrator<Runnable<MySampleTNode>> noname( "MySampleTemplateNode" );
