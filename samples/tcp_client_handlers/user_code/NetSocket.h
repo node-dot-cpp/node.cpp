a59fce382b6d40a5f2ca1c53608a177870f8a7c1
DIvanchykhin <di@o-log-n.com>
2020-01-23 12:31:27 +0200

    adding tcp-related samples (continuing)

--- samples/tcp_client_handlers/user_code/NetSocket.h
+++ samples/tcp_client_handlers/user_code/NetSocket.h
@@ -7,7 +7,6 @@
 #include <nodecpp/socket_common.h>
 
 using namespace nodecpp;
-using namespace fmt;
 
 #ifdef AUTOMATED_TESTING_ONLY
 #define AUTOMATED_TESTING_CYCLE_COUNT 30
@@ -15,110 +14,76 @@ using namespace fmt;
 
 class MySampleTNode : public NodeBase
 {
+	size_t recvSize = 0;
+	size_t recvReplies = 0;
+	Buffer buf;
+
+	using ClientSockType = net::SocketBase;
+
 public:
 	MySampleTNode() {}
 
-	virtual nodecpp::handler_ret_type main()
+	virtual handler_ret_type main()
 	{
-		log::default_log::info( log::ModuleID(nodecpp_module_id), "MySampleLambdaOneNode::main()" );
+		net::SocketBase::addHandler<ClientSockType, net::SocketBase::DataForCommandProcessing::UserHandlers::Handler::Connect, &MySampleTNode::onConnect>(this);
+		net::SocketBase::addHandler<ClientSockType, net::SocketBase::DataForCommandProcessing::UserHandlers::Handler::Data, &MySampleTNode::onData>(this);
 
-		nodecpp::net::SocketBase::addHandler<ClientSockType, nodecpp::net::SocketBase::DataForCommandProcessing::UserHandlers::Handler::Connect, &MySampleTNode::onConnect>(this);
-		nodecpp::net::SocketBase::addHandler<ClientSockType, nodecpp::net::SocketBase::DataForCommandProcessing::UserHandlers::Handler::Connect, &ClientSockType::onConnect>();
-
-		clientSock = nodecpp::net::createSocket<ClientSockType>();
-		*( clientSock->getExtra() ) = 17;
+		clientSock = net::createSocket<ClientSockType>();
 		clientSock->connect(2000, "127.0.0.1");
 		CO_RETURN;
 	}
 
-	class MySocketOne; // just forward declaration
-	nodecpp::handler_ret_type onConnect(nodecpp::safememory::soft_ptr<MySocketOne> socket) 
+	handler_ret_type onConnect(safememory::soft_ptr<net::SocketBase> socket) 
 	{
-		printf( "MySampleTNode::onConnect() with extra = %d\n", *(socket->getExtra()) );
-		CO_RETURN;
-	}
-
-	using ClientSockBaseType = nodecpp::net::SocketBase;
-
-	class MySocketOne : public ClientSockBaseType
-	{
-		size_t recvSize = 0;
-		size_t recvReplies = 0;
-		Buffer buf;
-		int extraData;
-
-	public:
-		MySocketOne() {}
-		virtual ~MySocketOne() {}
-
-		int* getExtra() { return &extraData; }
-
-		nodecpp::handler_ret_type onConnect() 
-		{
-			printf( "onConnect()\n" );
 			Buffer buf(2);
 			buf.writeInt8( 2, 0 );
 			buf.writeInt8( 1, 1 );
 			try
 			{
-				co_await a_write(buf);
-				co_await processIncomingData();
+				co_await socket->a_write(buf);
 			}
 			catch (...)
 			{
-				log::default_log::error( log::ModuleID(nodecpp_module_id), "Writing data failed (extra = {}). Exiting...", *(getExtra()));
-				// TODO: address failure
+				log::default_log::error( log::ModuleID(nodecpp_module_id), "Writing data failed). Exiting..." );
+				socket->end();
+				socket->unref();
 			}
 			CO_RETURN;
-		}
+	}
 
-		awaitable<void> processIncomingData()
-		{
-			for (;;)
-			{
-				nodecpp::Buffer r_buff(0x200);
-				try
-				{
-					co_await a_read(r_buff, (uint8_t)recvReplies | 1);
-				}
-				catch (...)
-				{
-					log::default_log::error( log::ModuleID(nodecpp_module_id), "Reading data failed (extra = {}). Exiting...", *(getExtra()));
-					break;
-				}
-				++recvReplies;
+	awaitable<void> onData(safememory::soft_ptr<net::SocketBase> socket, Buffer& r_buff )
+	{
+//		co_await socket->a_read(r_buff, (uint8_t)recvReplies | 1);
+		++recvReplies;
 #ifdef AUTOMATED_TESTING_ONLY
-				if ( recvReplies > AUTOMATED_TESTING_CYCLE_COUNT )
-				{
-					log::default_log::info( log::ModuleID(nodecpp_module_id), "About to exit successfully in automated testing" );
-					end();
-					unref();
-					break;
-				}
-#endif
-				if ( ( recvReplies & 0xFFF ) == 0 )
-					log::default_log::info( log::ModuleID(nodecpp_module_id), "[{}] MySampleTNode::onWhateverData(), size = {}, total received size = {}", recvReplies, r_buff.size(), recvSize );
-				recvSize += r_buff.size();
-				buf.writeInt8( 2, 0 );
-				buf.writeInt8( (uint8_t)recvReplies | 1, 1 );
-				try
-				{
-					co_await a_write(buf);
-				}
-				catch (...)
-				{
-					log::default_log::error( log::ModuleID(nodecpp_module_id), "Writing data failed (extra = {}). Exiting...", *(getExtra()));
-					break;
-				}
-				// TODO: address failure
-			}
+		if ( recvReplies > AUTOMATED_TESTING_CYCLE_COUNT )
+		{
+			log::default_log::info( log::ModuleID(nodecpp_module_id), "About to exit successfully in automated testing" );
+			socket->end();
+			socket->unref();
 			CO_RETURN;
 		}
-	};
+#endif
+		if ( ( recvReplies & 0xFFF ) == 0 )
+			log::default_log::info( log::ModuleID(nodecpp_module_id), "[{}] MySampleTNode::onWhateverData(), size = {}, total received size = {}", recvReplies, r_buff.size(), recvSize );
+		recvSize += r_buff.size();
+		buf.writeInt8( 2, 0 );
+		buf.writeInt8( (uint8_t)recvReplies | 1, 1 );
+		try
+		{
+			co_await socket->a_write(buf);
+		}
+		catch (...)
+		{
+			log::default_log::error( log::ModuleID(nodecpp_module_id), "Writing data failed. Exiting..." );
+			socket->end();
+			socket->unref();
+		}
 
-	using ClientSockType = MySocketOne;
+		CO_RETURN;
+	}
 
-	nodecpp::safememory::owning_ptr<ClientSockType> clientSock;
+	safememory::owning_ptr<ClientSockType> clientSock;
 };
 
 #endif // NET_SOCKET_H
