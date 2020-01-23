3d4b0016b2ecbac19e029435ff5ba4c71eb4c088
DIvanchykhin <di@o-log-n.com>
2020-01-23 11:03:56 +0200

    closing sample http server on a command (nodejs-based versions)

--- samples/http_server_handlers/nodejs/server_cluster.js
+++ samples/http_server_handlers/nodejs/server_cluster.js
@@ -21,14 +21,22 @@ if (cluster.isMaster) {
 } else {
   // Workers can share any TCP connection
   // In this case it is an HTTP server
-  http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type":"text/xml"});
-    var urlObj = url.parse(request.url, true);
-    var value = urlObj.query["value"];
-    if (value == ''){
-        response.end("no value specified");
+  var server = http.createServer(function(request, response) {
+    if ( request.method == "GET" || request.method == "HEAD" ) {
+        response.writeHead(200, {"Content-Type":"text/xml"});
+        var urlObj = url.parse(request.url, true);
+        var value = urlObj.query["value"];
+        if (value == ''){
+            response.end("no value specified");
+        } else if (value == 'close'){
+            server.close();
+            response.end("closing server...");
+        } else {
+            response.end("" + value + "");
+        }
     } else {
-        response.end("" + value + "");
+        response.writeHead(405, "Method Not Allowed");
+	response.end();
     }
   }).listen(2000);
 
