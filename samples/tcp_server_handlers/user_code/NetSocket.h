8a417acfc929621dafb3c5afe5576bcfa0294a8e
dmytro.ivanchykhin <di@o-log-n.com>
2020-01-27 11:35:00 +0200

    adding tcp-related samples (simplifying code)

--- samples/tcp_server_handlers/user_code/NetSocket.h
+++ samples/tcp_server_handlers/user_code/NetSocket.h
@@ -20,51 +20,10 @@ class MySampleTNode : public NodeBase
 	};
 	Stats stats;
 
-	Buffer replyBuff;
-
-	class MyWorkingSocket : public nodecpp::net::SocketBase
-	{
-	public:
-		using NodeType = MySampleTNode;
-		friend class MySampleTNode;
-
-	private:
-		MySampleTNode* myNode = nullptr;
-
-	public:
-		MyWorkingSocket() {}
-		virtual ~MyWorkingSocket() {}
-	};
-
-	class MyStatSocket : public nodecpp::net::SocketBase
-	{
-	public:
-		using NodeType = MySampleTNode;
-		friend class MySampleTNode;
-
-	private:
-		MySampleTNode* myNode = nullptr;
-
-	public:
-		MyStatSocket() {}
-		virtual ~MyStatSocket() {}
-	};
-
-	class MyWorkingServer : public nodecpp::net::ServerBase
-	{
-	public:
-		MyWorkingServer() {}
-//		MyWorkingServer(acceptedSocketCreationRoutineType socketCreationCB) : MyServerSocketBase(socketCreationCB) {};
-		virtual ~MyWorkingServer() {}
-	};
-
-	class MyStatServer : public nodecpp::net::ServerBase
-	{
-	public:
-		MyStatServer() {}
-//		MyStatServer(acceptedSocketCreationRoutineType socketCreationCB) : MyServerSocketBase(socketCreationCB) {};
-		virtual ~MyStatServer() {}
-	};
+	class MyWorkingSocket : public nodecpp::net::SocketBase {};
+	class MyStatSocket : public nodecpp::net::SocketBase {};
+	class MyWorkingServer : public nodecpp::net::ServerBase {};
+	class MyStatServer : public nodecpp::net::ServerBase {};
 
 public:
 	MySampleTNode() {}
@@ -98,31 +57,37 @@ public:
 
 	nodecpp::handler_ret_type onWorkingData(nodecpp::safememory::soft_ptr<MyWorkingSocket> socket, Buffer& buffer)
 	{
-		size_t receivedSz = buffer.begin()[0];
-		if (receivedSz != buffer.size())
+		if ( buffer.size() < 2 )
 		{
-			log::default_log::info( log::ModuleID(nodecpp_module_id), "Corrupted data on socket: received {}, expected: {} bytes", receivedSz, buffer.size());
-			socket->unref();
-			CO_RETURN;
+			//log::default_log::info( log::ModuleID(nodecpp_module_id), "Insufficient data on socket idx = %d", *extra );
+			socket->end();
+			return;
+		}
+	
+		if ( buffer.size() == 2 + 2 && buffer.readUInt8(2) == 0xfe && buffer.readUInt8(2) == 0xfe )
+		{
+			log::default_log::info( log::ModuleID(nodecpp_module_id), "Request to close server received" );
+			srv->close();
+			srvCtrl->close();
 		}
 
-		size_t requestedSz = buffer.begin()[1];
-		if (requestedSz)
+		size_t receivedSz = buffer.readUInt8(0);
+		if ( receivedSz != buffer.size() )
+		{
+			log::default_log::info( log::ModuleID(nodecpp_module_id), "Corrupted data on socket idx = [??]: received {}, expected: {} bytes", receivedSz, buffer.size() );
+			socket->unref();
+			return;
+		}
+	
+		size_t requestedSz = buffer.readUInt8(1);
+		if ( requestedSz )
 		{
 			Buffer reply(requestedSz);
-			reply.set_size(requestedSz);
-			memset(reply.begin(), (uint8_t)requestedSz, requestedSz);
-			try
-			{
-				co_await socket->a_write(reply);
-			}
-			catch (...)
-			{
-				log::default_log::error( log::ModuleID(nodecpp_module_id), "Writing data failed. Exiting...");
-				CO_RETURN;
-			}
+			for ( size_t i=0; i<(uint8_t)requestedSz; ++i )
+				reply.appendUint8( 0 );
+			socket->write(reply);
 		}
-
+	
 		stats.recvSize += receivedSz;
 		stats.sentSize += requestedSz;
 		++(stats.rqCnt);
@@ -140,23 +105,14 @@ public:
 
 	nodecpp::handler_ret_type onStatData(nodecpp::safememory::soft_ptr<MyStatSocket> socket, Buffer& buffer)
 	{
-		Buffer reply(sizeof(stats));
-
 		size_t requestedSz = buffer.begin()[1];
 		if (requestedSz)
 		{
 			stats.connCnt = srv->getSockCount();
+			Buffer reply(sizeof(stats));
 			size_t replySz = sizeof(Stats);
-			reply.clear();
 			reply.append(&stats, replySz);
-			try
-			{
-				co_await socket->a_write(reply);
-			}
-			catch (...)
-			{
-				log::default_log::error( log::ModuleID(nodecpp_module_id), "Writing data failed). Exiting..." );
-			}
+			co_await socket->a_write(reply);
 		}
 	}
 
