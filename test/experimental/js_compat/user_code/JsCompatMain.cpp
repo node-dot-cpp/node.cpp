f7d70d6d1def7dfdab1b73a9dc1856d5e513535f
ZelenskiyS <semen.zelenskiy@gmail.com>
2020-05-01 13:14:39 +0300

    js compatibility layer (add implemantation for random.js)

--- test/experimental/js_compat/user_code/JsCompatMain.cpp
+++ test/experimental/js_compat/user_code/JsCompatMain.cpp
@@ -5,6 +5,11 @@
 #include <nodecpp/js_compat.h>
 #include "colors.h"
 #include "../js-compat-tests/misc_tests.h"
+#include "./maps/america.h"
+#include "./maps/rainbow.h"
+#include "./maps/random.h"
+#include "./maps/zebra.h"
+#include "./custom/trap.h"
 
 int main( int argc, char *argv_[] )
 {
@@ -18,6 +23,22 @@ int main( int argc, char *argv_[] )
 	nodecpp::logging_impl::currentLog = &log;
 	nodecpp::logging_impl::instanceId = 0;*/
 
+	JSVar america = require<America>();
+	printf("\n~~~~~~~~~~~~~~~\n%s\n~~~~~~~~~~~~~~~\n\n", america.toString().c_str());
+
+	JSVar rainbow = require<Rainbow>();
+	printf("\n~~~~~~~~~~~~~~~\n%s\n~~~~~~~~~~~~~~~\n\n", rainbow.toString().c_str());
+
+	JSVar random = require<Random>();
+	printf(random("\n RANDOM TEST \n").toString().c_str());
+
+	JSVar zebra = require<Zebra>();
+	printf(zebra("\n ZEBRA TEST \n").toString().c_str());
+
+	//JSVar trap = require<Trap>();
+	//printf(trap("TRAP TEST\n").toString().c_str());
+
+
 	JSVar styles = require<Styles>();
 	printf( "\n~~~~~~~~~~~~~~~\n%s\n~~~~~~~~~~~~~~~\n\n", styles.toString().c_str() );
 
@@ -27,6 +48,10 @@ int main( int argc, char *argv_[] )
 	auto& miscTests = import<MiscTests>();
 //	printf( "\n~~~~~~~~~~~~~~~%s\n~~~~~~~~~~~~~~~\n\n", miscTests.toString().c_str() );
 
+
+	//JS TEST
+	/*require('./extendStringPrototype')();
+	console.log('drop the bass'.trap);*/
 	return 0;
 }
 
---
+++ test/experimental/js_compat/user_code/maps/random.h
@@ -0,0 +1,67 @@
+/* -------------------------------------------------------------------------------
+* Copyright (c) 2020, OLogN Technologies AG
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the OLogN Technologies AG nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL OLogN Technologies AG BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* -------------------------------------------------------------------------------*/
+
+#ifndef RANDOM_H
+#define RANDOM_H
+#include <js_compat.h>
+
+
+using namespace nodecpp::js;
+
+class Random : public nodecpp::js::JSModule
+{
+private:
+    JSOwnObj avaible_ = makeJSArray({ "underline", "inverse", "grey", "yellow", "red", "green","blue",
+                                  "white", "cyan", "magenta", "brightYellow", "brightRed","brightGreen", 
+                                  "brightBlue", "brightWhite", "brightCyan", "brightMagenta" });
+public:
+    JSVar exports() {
+        JSVar avaible = avaible_;
+        return
+        JSVar([avaible](JSVar colors) {
+            return
+                JSVar([colors, avaible](JSVar letter, JSVar i) {
+                return letter.isStrictlyTheSame(" ") ? letter : colors[avaible[JSMath::random() * (avaible.length() - 2)]];
+                    });
+            });
+    };
+};
+
+// 
+#endif // RANDOM_H
+
+/*module['exports'] = function(colors) {
+    var available = ['underline', 'inverse', 'grey', 'yellow', 'red', 'green',
+        'blue', 'white', 'cyan', 'magenta', 'brightYellow', 'brightRed',
+        'brightGreen', 'brightBlue', 'brightWhite', 'brightCyan', 'brightMagenta'];
+    return function(letter, i, exploded) {
+        return letter == = ' ' ? letter :
+            colors[
+                available[Math.round(Math.random() * (available.length - 2))]
+            ](letter);
+    };
+};*/
