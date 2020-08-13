6bc0beb6397cb48885d669b947733f4300b5919d
Marcos Bracco <mb@o-log-n.com>
2020-08-13 17:45:22 -0300

    update safe_memory

--- include/nodecpp/common.h
+++ include/nodecpp/common.h
@@ -65,6 +65,14 @@ namespace nodecpp {
 #include <array>
 #include <algorithm>
 
+#include <malloc_based_allocator.h>
+#include <safe_memory/vector.h>
+#include <safe_memory/unordered_map.h>
+#include <safe_memory/string.h>
+#include <safe_memory/string_literal.h>
+#include <safe_memory/string_format.h>
+
+
 //#include <cassert>
 //#include <fmt/format.h>
 //#include "trace.h"
@@ -84,41 +92,50 @@ using GlobalObjectAllocator = Mallocator<T>;
 
 namespace nodecpp
 {
+
+	// template<class T>
+	// using vector = ::safe_memory::vector<T>;
 	template<class T>
 	using vector = ::std::vector<T, nodecpp::safememory::iiballocator<T>>;
 
 	template<class T>
 	using stdvector = ::std::vector<T, nodecpp::stdallocator<T>>;
 
+	// template<class Key, class T>
+	// using map = ::safe_memory::unordered_map<Key, T>;
 	template<class Key, class T>
 	using map = ::std::map<Key, T, std::less<Key>, nodecpp::safememory::iiballocator<std::pair<const Key,T>>>;
 
 	template<class Key, class T>
 	using stdmap = ::std::map<Key, T, std::less<Key>, nodecpp::stdallocator<std::pair<const Key,T>>>;
 
+	// using string = ::safe_memory::string;
 	using string = ::std::basic_string<char, std::char_traits<char>, nodecpp::safememory::iiballocator<char>>;
 
 	using stdstring = ::std::basic_string<char, std::char_traits<char>, nodecpp::stdallocator<char>>;
 
-	class string_literal
-	{
-		const char* str;
-	public:
-		string_literal() : str( nullptr ) {}
-		string_literal( const char* str_) : str( str_ ) {}
-		string_literal( const string_literal& other ) : str( other.str ) {}
-		string_literal& operator = ( const string_literal& other ) {str = other.str; return *this;}
-		string_literal( string_literal&& other ) : str( other.str ) {}
-		string_literal& operator = ( string_literal&& other ) {str = other.str; return *this;}
+	using string_literal = ::safe_memory::string_literal;
 
-		bool operator == ( const string_literal& other ) const { return strcmp( str, other.str ) == 0; }
-		bool operator != ( const string_literal& other ) const { return strcmp( str, other.str ) != 0; }
+// 	using 
+// 	class string_literal
+// 	{
+// 		const char* str;
+// 	public:
+// 		string_literal() : str( nullptr ) {}
+// 		string_literal( const char* str_) : str( str_ ) {}
+// 		string_literal( const string_literal& other ) : str( other.str ) {}
+// 		string_literal& operator = ( const string_literal& other ) {str = other.str; return *this;}
+// 		string_literal( string_literal&& other ) : str( other.str ) {}
+// 		string_literal& operator = ( string_literal&& other ) {str = other.str; return *this;}
 
-//		bool operator == ( const char* other ) const { return strcmp( str, other.str ) == 0; }
-//		bool operator != ( const char* other ) const { return strcmp( str, other.str ) != 0; }
+// 		bool operator == ( const string_literal& other ) const { return strcmp( str, other.str ) == 0; }
+// 		bool operator != ( const string_literal& other ) const { return strcmp( str, other.str ) != 0; }
 
-		const char* c_str() const { return str; }
-	};
+// //		bool operator == ( const char* other ) const { return strcmp( str, other.str ) == 0; }
+// //		bool operator != ( const char* other ) const { return strcmp( str, other.str ) != 0; }
+
+// 		const char* c_str() const { return str; }
+// 	};
 
 	template <typename... Args>
 	inline nodecpp::string format(
