ec5bfd354d33abbe33ae651638d7039b0acde826
dmytro.ivanchykhin <di@o-log-n.com>
2020-10-06 11:29:56 +0300

    pre_marshalling (updated sample)

--- test/experimental/named_fn_params/user_code/named_fn_params.cpp
+++ test/experimental/named_fn_params/user_code/named_fn_params.cpp
@@ -6,7 +6,7 @@
 #include "../generated/marshalling.h"
 //#include "../manual/test.h"
 
-using m::Parser;
+//using m::Parser;
 
 struct Point
 {
@@ -21,6 +21,7 @@ struct Point3D
 	int z;
 };
 
+#if 0
 class VectorOfPointsWrapper : public m::CollectionWrapperBase
 {
 	nodecpp::vector<Point>& coll;
@@ -90,7 +91,7 @@ public:
 		coll.push_back( pt );
 	}
 };
-
+#endif // 0
 int main()
 {
 #if 0
@@ -245,35 +246,41 @@ int main()
 		nodecpp::vector<int> vectorOfNumbers = { 0, 1, 2, 3, 4, 5 };
 		nodecpp::vector<Point> vectorOfPoints;
 		nodecpp::vector<Point3D> vectorOfPoints3D = { {0, 1, 2}, {3, 4, 5} };
-		auto point3Dit = vectorOfPoints3D.begin();
 		nodecpp::vector<int> vectorOfNumbersBack;
 		nodecpp::vector<Point> vectorOfPointsBack;
 		nodecpp::vector<Point3D> vectorOfPoints3DBack;
 		nodecpp::Buffer b;
 		m::Composer composer( m::Proto::GMQ, b );
-//		m::message_one_compose( composer, m::sixthParam = m::CollectionWrapperForComposing( vectorOfPoints3D/*, [&vectorOfPoints3D]() { return vectorOfPoints3D.size(); }*/, [](m::Composer c, Point3D& p){m::point3D_compose( c, m::x = p.x, m::y = p.y, m::z = p.z );} ), m::firstParam = 1, m::secondParam = nodecpp::string("def"), m::thirdParam = 3, m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbers ) );
-		m::message_one_compose( composer, m::sixthParam = m::CollectionWrapperForComposing( vectorOfPoints3D/*, [&vectorOfPoints3D]() { return vectorOfPoints3D.size(); }*/, [&vectorOfPoints3D, &point3Dit](m::Composer c){ if ( point3Dit != vectorOfPoints3D.end() ) { m::point3D_compose( c, m::x = point3Dit->x, m::y = point3Dit->y, m::z = point3Dit->z ); ++point3Dit; return point3Dit != vectorOfPoints3D.end();} return false;} )/*, m::firstParam = 1, m::secondParam = nodecpp::string("def"), m::thirdParam = 3, m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbers )*/ );
+		m::message_one_compose( composer, 
+			m::sixthParam = m::CollectionWrapperForComposing( [&vectorOfPoints3D]() { return vectorOfPoints3D.size(); }, [&vectorOfPoints3D](m::Composer& c, size_t ordinal){ m::point3D_compose( c, m::x = vectorOfPoints3D[ordinal].x, m::y = vectorOfPoints3D[ordinal].y, m::z = vectorOfPoints3D[ordinal].z );} ), 
+			m::firstParam = 1, m::secondParam = nodecpp::string("def"), m::thirdParam = 3, 
+			m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbers ) );
 
-		Parser parser( m::Proto::GMQ, b.begin(), b.size() );
+		m::Parser parser( m::Proto::GMQ, b.begin(), b.size() );
 		int firstParam = -1;
 		nodecpp::string secondParam = "";
 		int thirdParam = -1;
-		m::message_one_parse( parser, m::firstParam = &firstParam, m::thirdParam = &thirdParam, m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbersBack ), m::secondParam = &secondParam, m::fifthParam = VectorOfPointsWrapper( vectorOfPointsBack ), m::sixthParam = VectorOfPoints3DWrapper( vectorOfPoints3DBack ) );
+		m::message_one_parse( parser, 
+			m::firstParam = &firstParam, m::thirdParam = &thirdParam, 
+			m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbersBack ), 
+			m::sixthParam = m::CollactionWrapperForParsing( nullptr, [&vectorOfPoints3DBack](m::Parser& p, size_t ordinal){ Point3D pt; m::point3D_parse( p, m::x = &(pt.x), m::y = &(pt.y), m::z = &(pt.z) ); vectorOfPoints3DBack.push_back( pt );} ), 
+			m::fifthParam = m::CollactionWrapperForParsing( [&vectorOfPointsBack](size_t sz){vectorOfPointsBack.reserve( sz );}, [&vectorOfPointsBack](m::Parser& p, size_t ordinal){ Point pt; m::point_parse( p,  m::x = &(pt.x), m::y = &(pt.y) ); vectorOfPointsBack.push_back( pt );} ), 
+			m::secondParam = &secondParam );
 
-		/*NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, firstParam == 1, "Indeed: {}", firstParam );
+		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, firstParam == 1, "Indeed: {}", firstParam );
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, secondParam == "def", "Indeed: {}", secondParam );
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, thirdParam == 3, "Indeed: {}", thirdParam );
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfNumbers.size() == vectorOfNumbersBack.size(), "{} vs {}", vectorOfNumbers.size(), vectorOfNumbersBack.size() );
 		for ( size_t i=0; i<vectorOfNumbers.size(); ++i )
-			NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfNumbers[i] == vectorOfNumbersBack[i], "{} vs {}", vectorOfNumbers[i], vectorOfNumbersBack[i] );*/
+			NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfNumbers[i] == vectorOfNumbersBack[i], "{} vs {}", vectorOfNumbers[i], vectorOfNumbersBack[i] );
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfPointsBack.size() == 0, "indeed: {}", vectorOfPointsBack.size() );
-		/*NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfPoints3DBack.size() == vectorOfPoints3D.size(), "{} vs {}", vectorOfPoints3DBack.size(), vectorOfPoints3D.size() );
+		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfPoints3DBack.size() == vectorOfPoints3D.size(), "{} vs {}", vectorOfPoints3DBack.size(), vectorOfPoints3D.size() );
 		for ( size_t i=0; i<vectorOfPoints3D.size(); ++i )
 		{
 			NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfPoints3D[i].x == vectorOfPoints3DBack[i].x, "{} vs {}", vectorOfPoints3D[i].x, vectorOfPoints3DBack[i].x );
 			NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfPoints3D[i].y == vectorOfPoints3DBack[i].y, "{} vs {}", vectorOfPoints3D[i].y, vectorOfPoints3DBack[i].y );
 			NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, vectorOfPoints3D[i].z == vectorOfPoints3DBack[i].z, "{} vs {}", vectorOfPoints3D[i].z, vectorOfPoints3DBack[i].z );
-		}*/
+		}
 	}
 
 	{
@@ -285,15 +292,23 @@ int main()
 		nodecpp::Buffer b;
 		m::Composer composer( m::Proto::JSON, b );
 
-		m::message_one_compose( composer, m::firstParam = 1, m::secondParam = nodecpp::string("def"), m::thirdParam = 3, m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbers ), m::sixthParam = VectorOfPoints3DWrapper( vectorOfPoints3D ) );
+		m::message_one_compose( composer, 
+			m::sixthParam = m::CollectionWrapperForComposing( [&vectorOfPoints3D]() { return vectorOfPoints3D.size(); }, [&vectorOfPoints3D](m::Composer& c, size_t ordinal){ m::point3D_compose( c, m::x = vectorOfPoints3D[ordinal].x, m::y = vectorOfPoints3D[ordinal].y, m::z = vectorOfPoints3D[ordinal].z );} ), 
+			m::firstParam = 1, m::secondParam = nodecpp::string("def"), m::thirdParam = 3, 
+			m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbers ) );
 		b.appendUint8( 0 );
 		printf( "%s\n", b.begin() );
 
-		Parser parser( m::Proto::JSON, b.begin(), b.size() );
+		m::Parser parser( m::Proto::JSON, b.begin(), b.size() );
 		int firstParam = -1;
 		nodecpp::string secondParam = "";
 		int thirdParam = -1;
-		m::message_one_parse( parser, m::firstParam = &firstParam, m::secondParam = &secondParam, m::thirdParam = &thirdParam, m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbersBack ), m::fifthParam = VectorOfPointsWrapper( vectorOfPointsBack ), m::sixthParam = VectorOfPoints3DWrapper( vectorOfPoints3DBack ) );
+		m::message_one_parse( parser, 
+			m::firstParam = &firstParam, m::secondParam = &secondParam, m::thirdParam = &thirdParam, 
+			m::forthParam = m::SimpleTypeCollectionWrapper( vectorOfNumbersBack ), 
+			m::sixthParam = m::CollactionWrapperForParsing( nullptr, [&vectorOfPoints3DBack](m::Parser& p, size_t ordinal){ Point3D pt; m::point3D_parse( p, m::x = &(pt.x), m::y = &(pt.y), m::z = &(pt.z) ); vectorOfPoints3DBack.push_back( pt );} ), 
+			m::fifthParam = m::CollactionWrapperForParsing( [&vectorOfPointsBack](size_t sz){vectorOfPointsBack.reserve( sz );}, [&vectorOfPointsBack](m::Parser& p, size_t ordinal){ Point pt; m::point_parse( p,  m::x = &(pt.x), m::y = &(pt.y) ); vectorOfPointsBack.push_back( pt );} )
+		);
 
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, firstParam == 1, "Indeed: {}", firstParam );
 		NODECPP_ASSERT( nodecpp::module_id, ::nodecpp::assert::AssertLevel::critical, secondParam == "def", "Indeed: {}", secondParam );
