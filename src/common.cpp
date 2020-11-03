923ec56da4f4cdd1bbdbc36d206b1727694bef25
dmytro.ivanchykhin <di@o-log-n.com>
2020-11-03 14:38:37 +0200

    toward alt infras and main loops (moving lib code to respective locations)

---
+++ src/common.cpp
@@ -0,0 +1,44 @@
+/* -------------------------------------------------------------------------------
+* Copyright (c) 2020, OLogN Technologies AG
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the OLogN Technologies AG nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL OLogN Technologies AG BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* -------------------------------------------------------------------------------*/
+
+#include "common.h"
+#include "../include/nodecpp/nls.h"
+
+#ifdef NODECPP_ENABLE_ONSTACK_SOFTPTR_COUNTING
+thread_local size_t nodecpp::safememory::onStackSafePtrCreationCount; 
+thread_local size_t nodecpp::safememory::onStackSafePtrDestructionCount;
+#endif // NODECPP_ENABLE_ONSTACK_SOFTPTR_COUNTING
+thread_local void* nodecpp::safememory::thg_stackPtrForMakeOwningCall = 0;
+
+#if defined NODECPP_USE_NEW_DELETE_ALLOC
+thread_local void** nodecpp::safememory::zombieList_ = nullptr;
+#ifndef NODECPP_DISABLE_ZOMBIE_ACCESS_EARLY_DETECTION
+thread_local std::map<uint8_t*, size_t, std::greater<uint8_t*>> nodecpp::safememory::zombieMap;
+thread_local bool nodecpp::safememory::doZombieEarlyDetection_ = true;
+#endif // NODECPP_DISABLE_ZOMBIE_ACCESS_EARLY_DETECTION
+#endif // NODECPP_USE_xxx_ALLOC
+
